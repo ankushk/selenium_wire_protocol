c4ce1ea54f35f54e2ced56e6649d5f9f63bd9340
Ankush Kalkote <ankush@Ankushs-MacBook-Air.local>
2014-08-18 14:37:06 +0530

    Implemented selenium json protocol in shell

---
+++ selenium-wire.sh
@@ -0,0 +1,60 @@
+
+#!/bin/bash
+
+SELENIUM_SERVER="http://127.0.0.1:4444/wd/hub"
+testing="{"status":0,"sessionId":"e2fd77dc-fbfd-4f9e-8160-d86b313abf80","value":{"platform":"MAC","acceptSslCerts":true,"javascriptEnabled":true,"browserName":"firefox","rotatable":false,"locationContextEnabled":true,"webdriver.remote.sessionid":"e2fd77dc-fbfd-4f9e-8160-d86b313abf80","version":"31.0","cssSelectorsEnabled":true,"databaseEnabled":true,"handlesAlerts":true,"nativeEvents":false,"webStorageEnabled":true,"applicationCacheEnabled":true,"takesScreenshot":true},"state":null,"class":"org.openqa.selenium.remote.Response","hCode":107865609}"
+
+getParam () {
+  if [ "$#" -ne 2 ]; then
+    echo "Function Usage: getParam JSON_STRING PARAM_NAME " >&2
+    exit 1
+  fi
+  ret_val=$(echo $1 | tr ',{}' '\n' | grep $2 | awk -F: '{printf $2}')
+  echo $ret_val
+}
+
+checkStatus () {
+  if [ "$#" -ne 2 ]; then
+    echo "Function Usage: checkStatus JSON_STRING ACTION" >&2
+    exit 1
+  fi
+  status=$(getParam $1 "status")
+  if [ $status -ne "0" ]; then
+    echo "Status is non-zero, error-code: $status" >&2
+    echo "$2 failed" >&2
+    exit 1
+  fi
+}
+
+# Create a session
+response=`curl -X POST -d @browser-caps.json $SELENIUM_SERVER/session`
+checkStatus $response "Session creation"
+session_id=$(getParam $response "sessionId" | sed "s/\"//g") 
+
+echo "SessionId is $session_id"
+
+# Open url
+response=`curl -X POST -d @url.json $SELENIUM_SERVER/session/$session_id/url`
+echo "Response: $response"
+checkStatus $response "Opening URL"
+
+# Find search element
+response=`curl -X POST -d @element.json $SELENIUM_SERVER/session/$session_id/element`
+checkStatus $response "Finding element"
+element_id=$(getParam $response "ELEMENT" | sed "s/\"//g")
+
+# Send keystrokes
+response=`curl -X POST -d "{"value":["Browserstack"]}" $SELENIUM_SERVER/session/$session_id/element/$element_id/value`
+checkStatus $response "Sending keystrokes"
+sleep 3
+
+# Get the title
+response=`curl $SELENIUM_SERVER/session/$session_id/title`
+checkStatus $response "Getting title"
+title=$(getParam $response "value")
+echo "Title of the window: $title"
+
+# Delete the session
+response=`curl -X DELETE $SELENIUM_SERVER/session/$session_id`
+checkStatus $response "Closing the session"
+
